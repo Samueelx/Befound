62de4633d4e09fa869829f7f51ffef77554cc5dc
Samueelx <gsammygitau@gmail.com>
2023-10-06 12:51:41 +0300

    Initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold text-center mt-2">
+          Befound
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
