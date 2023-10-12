35598392e5f670952ecda7af4ebfc3cb14ff4858
Samueelx <gsammygitau@gmail.com>
2023-10-12 16:36:15 +0300

    fix ErrorPage error where the error variable  was of type unknown.

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,15 +1,31 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError, isRouteErrorResponse } from "react-router-dom";
 
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
+
+  function errorMessage(error: unknown): string {
+    if (isRouteErrorResponse(error)) {
+      return `${error.status} ${error.statusText}`
+    } else if (error instanceof Error) {
+      return error.message
+    } else if (typeof error === 'string') {
+      return error
+    } else {
+      console.error(error)
+      return 'Unknown error'
+    }
+  }
 
   return (
     <div id="error-page" className="flex flex-col items-center justify-center">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{error.statusText || error.message}</i>
+        <i>
+          {/* {error.statusText || error.message} */}
+          {errorMessage(error)}
+        </i>
       </p>
     </div>
   );
