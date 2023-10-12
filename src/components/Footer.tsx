5b73344efcf979720b5f6ec00f4e158f1b37cf8a
Samueelx <gsammygitau@gmail.com>
2023-10-12 16:19:25 +0300

    set up routing and add mobile classes to style the footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,16 +3,16 @@ import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
 
 const Footer: React.FC = () => {
   return (
-    <section className="bg-slate-900 flex justify-around items-center h-32 text-white">
-      <div>
+    <section className="bg-slate-900 flex flex-col md:flex-row md:justify-around items-center justify-evenly h-48 md:h-32 text-white">
+      <div className="my-2">
         <span className="text-3xl font-bold">Befound</span>
       </div>
-      <div>
+      <div className="my-2">
         <p className="font-extralight">
           Copyright<span>&#169;</span>Befound <span>2023</span>
         </p>
       </div>
-      <div className="flex gap-8">
+      <div className="flex md:flex-row md:gap-8 gap-4">
         <a href="https://instagram.com/">
           <FaInstagram />
         </a>
